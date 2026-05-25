87895bc64dbe3a20f5d34eab42f0c8b14692c2ba
rostislav-nikitin90 <rosnikitin90@gmail.com>
2026-05-25 18:19:05 +0200

    Completed W04 Learning Activities

---
+++ week04/greeter.js
@@ -0,0 +1,18 @@
+"use strict";
+class Student {
+    firstName;
+    middleInitial;
+    lastName;
+    fullName;
+    constructor(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+let user = new Student("Jane", "M.", "User");
+document.body.textContent = greeter(user);
